a2e131e8eef5d13b280b8123b806b30d30de0c54
Max <amclauss1@gmail.com>
2013-01-29 01:12:08 -0500

    added processing code to graph time/temp profile

--- reflow/reflow.ino
+++ reflow/reflow.ino
@@ -42,37 +42,48 @@ int relay_state;       // whether the relay pin is high (on) or low (off)
 
 volatile long uptime;
 int opmode = 3;//opmode  is  0standby 1preheat 2ready 3on 
-#define phases 11
 
 //source for mapping  http://interface.khm.de/index.php/lab/experiments/nonlinear-mapping/
-float nodepoints[phases][2]= {
+float nodepoints[][2]= {
   {
-    -60,100      }
+    -60,100        }
   ,{
-    0,100      }
+    0,100        }
   ,{
-    15,150          } 
+    15,150            } 
   , {
-    105,190          }
+    105,190            }
   , {
-    120,220          }
+    120,220            }
   ,{
-    135,240          }
+    135,240            }
   ,{
-    150,220    }
+    150,220      }
   ,{
-    165,190    }
+    165,190      }
   ,{
-    180,160    }
+    180,160      }
   ,{
-    181,0    }
+    181,1      }
   ,{
-    480,0    }
+    480,1      }
+  ,
+  {
+    481,1  }
 };
+int phases = sizeof(nodepoints)/8;
 
 void setup() {  
-  Serial.begin(9600); 
-  Serial.println("Reflowduino!");
+  Serial.begin(115200); 
+  Serial.println("Reflowduino");
+  Serial.print("We have ");
+  Serial.print(phases);
+  Serial.print(" nodes spanning ");
+  Serial.print(nodepoints[phases-1][0]/60);
+  Serial.println(" mins  ");
+  Serial.println("Raw dump start:");
+  serialdump();
+  Serial.println("Draw");
 
   // The data header (we have a bunch of data to track)
   Serial.print("Time (s)\tTemp (C)\tSet Temp\tError\tSlope\tSummation\tPID Controller\tRelay");
@@ -117,6 +128,13 @@ void setup() {
   TIMSK1 = _BV(OCIE1A);                          // turn on interrupt
 }
 
+void serialdump(){//dump a graph of our temp/time profile to processing
+  for(int i=0;i<nodepoints[phases-1][0];i++){
+    Serial.println(reMap(nodepoints,i));
+    delay(10);
+  }
+  
+}
 
 void loop() { 
   // we moved the LCD code into the interrupt so we don't have to worry about updating the LCD 
@@ -223,11 +241,11 @@ int reMap(float pts[10][2], int input) {
 
     if (input >= pts[n][0] && input <= pts[n+1][0]) {
 
-        m = ( pts[n+1][1] - pts[n][1] ) / ( pts[n+1][0] - pts[n][0] );
-        m = m * (input-pts[n][0]);
-        m = m +  pts[n][1];
-  
-   
+      m = ( pts[n+1][1] - pts[n][1] ) / ( pts[n+1][0] - pts[n][0] );
+      m = m * (input-pts[n][0]);
+      m = m +  pts[n][1];
+
+
       //   m= ( pts[n][1] - pts[n+1][1] ) / ( pts[n][0] - pts[n+1][0] );
       //    m = m * (input-pts[n][0]);
       //   m = m +  pts[n][1];
@@ -239,6 +257,100 @@ int reMap(float pts[10][2], int input) {
   return(r);
 }
 
+/* PROCESSING CODE
+ 
+ 
+ 
+ // Graphing sketch
+ 
+ 
+ // This program takes ASCII-encoded strings
+ // from the serial port at 9600 baud and graphs them. It expects values in the
+ // range 0 to 1023, followed by a newline, or newline and carriage return
+ 
+ // Created 20 Apr 2005
+ // Updated 18 Jan 2008
+ // by Tom Igoe
+ // This example code is in the public domain.
+ 
+ import processing.serial.*;
+ 
+ Serial myPort;        // The serial port
+ int xPos = 1;         // horizontal position of the graph
+ 
+ void setup () {
+    textFont(createFont("Georgia", 24));
+ // set the window size:
+ size(800, 600);        
+ drawgraph();
+ delay(100);
+ // List all the available serial ports
+ println(Serial.list());
+ // I know that the first port in the serial list on my mac
+ // is always my  Arduino, so I open Serial.list()[0].
+ // Open whatever port is the one you're using.
+ myPort = new Serial(this, Serial.list()[0], 115200);
+ // don't generate a serialEvent() unless you get a newline character:
+ myPort.bufferUntil('\n');
+ // set inital background:
+ background(0);
+ }
+ void draw () {
+ // everything happens in the serialEvent()
+ delay(5000);
+drawgraph();
+ 
+ }
+ 
+ 
+ void serialEvent (Serial myPort) {
+ // get the ASCII string:
+ String inString = myPort.readStringUntil('\n');
+ 
+ if (inString != null) {
+   if(inString=="Draw"){
+    drawgraph(); 
+   }
+ // trim off any whitespace:
+ inString = trim(inString);
+ // convert to an int and map to the screen height:
+ float inByte = float(inString); 
+ inByte = map(inByte, 0, 270, 0, height);
+ 
+ // draw the line:
+ stroke(127,34,255);
+ line(xPos*2%width, height, xPos*2%width, height - inByte);
+ 
+ // at the edge of the screen, go back to the beginning:
+ if (xPos >= width) {
+ xPos = 0;
+ background(0); 
+ } 
+ else {
+ // increment the horizontal position:
+ xPos++;
+ }
+ }
+ }
+ 
+ void drawgraph(){
+   
+    stroke(157);
+ for(int i=1;i<30;i++){
+   stroke(157);line(0,map(i*10, 0, 270, height,0),800,map(i*10, 0, 270, height,0));
+   stroke(157);line(i*20,0,i*20,width);
+  stroke(157);text(i*10+"C", width * 0.75, map(i*10, 0, 270, height,0));
+ if(i%6==0){
+  stroke(157); line(i*20+1,0,i*20+1,width);
+  stroke(157); line(i*20-1,0,i*20-1,width);
+   stroke(157); text(i/6+"min", i*20, 50);
+  }
+ stroke(157); text("Reflowduino Reflow solder profile",0,25);
+}
+ }
+ 
+ */
+ 
 
 
 
